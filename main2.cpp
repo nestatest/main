dfc59d1a0934404b7b91f495593d309eab74ad70
nestatest <kostya.dovgal.99@bk.ru>
2025-04-04 11:07:56 +0700

    изменения

--- main2.cpp
+++ main2.cpp
@@ -1,22 +1,28 @@
 #include <iostream>
-#include <math.h>
+#include <cmath>
 
 using namespace std;
 
-// Проверка данных для трапеции
-int check_trapezoid(int a, int b, int c, int d, int h) {
-    if (a <= 0 || b <= 0 || c <= 0 || d <= 0 || h <= 0) {
-        cout << "Error, digit less than zero. Retry again\n";
+// Проверка корректности данных
+int check_trapezoid(int topbase, int bottombase, int leftside, int rightside, int height) {
+    if (topbase <= 0 || bottombase <= 0 || leftside <= 0 || rightside <= 0 || height <= 0) {
+        cout << "Error: all values must be greater than 0.\n";
         return 1;
     }
+
+    // Проверка: боковые стороны должны быть длиннее высоты
+    if (leftside <= height || rightside <= height) {
+        cout << "Error: trapezoid cannot exist. Side must be longer than height.\n";
+        return 1;
+    }
+
     return 0;
 }
 
-// Функция для расчёта параметров трапеции
+// Расчёт параметров трапеции
 void calculate_trapezoid() {
     int leftside, topbase, rightside, bottombase, height;
 
-    // Ввод данных с проверкой
     do {
         cout << "Left side: ";
         cin >> leftside;
@@ -28,14 +34,12 @@ void calculate_trapezoid() {
         cin >> bottombase;
         cout << "Height: ";
         cin >> height;
-    } while (check_trapezoid(leftside, topbase, rightside, bottombase, height));
+    } while (check_trapezoid(topbase, bottombase, leftside, rightside, height));
 
-    // Вычисления
     int perimeter = leftside + topbase + rightside + bottombase;
     double square = ((topbase + bottombase) / 2.0) * height;
     double midline = (topbase + bottombase) / 2.0;
 
-    // Вывод результатов
     cout << "Perimeter: " << perimeter << endl;
     cout << "Square: " << square << endl;
     cout << "Midline length: " << midline << endl;
@@ -45,4 +49,3 @@ int main() {
     calculate_trapezoid();
     return 0;
 }
-
